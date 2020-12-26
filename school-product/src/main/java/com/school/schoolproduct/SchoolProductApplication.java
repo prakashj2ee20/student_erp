de9f7e5a18f0f8a7584669187cc91385176e4f47
prakashj2ee20 <prakashj2ee20@gmail.com>
2020-12-26 07:16:18 -0800

    first commit
    Signed-off-by: prakashj2ee20 <prakashj2ee20@gmail.com>

---
+++ school-product/src/main/java/com/school/schoolproduct/SchoolProductApplication.java
@@ -0,0 +1,13 @@
+package com.school.schoolproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolProductApplication.class, args);
+	}
+
+}
